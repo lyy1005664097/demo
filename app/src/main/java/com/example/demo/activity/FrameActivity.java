5ca29ff1efd7c2ff766452dd1e862ed3fcbf350f
10056 <1005664097@qq.com>
2019-12-16 13:40:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demo/activity/FrameActivity.java
@@ -0,0 +1,80 @@
+package com.example.demo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+
+import com.example.demo.R;
+import com.wulianwang.lsp.fragment.AndroidFragment;
+import com.wulianwang.lsp.fragment.AppleFragment;
+
+public class FrameActivity extends AppCompatActivity implements View.OnClickListener {
+
+    AppleFragment appleFragment;
+    AndroidFragment androidFragment;
+    FragmentManager fm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame);
+
+        initView();
+    }
+
+    private void initView(){
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        RadioButton rb1 = findViewById(R.id.bt1);
+        RadioButton rb2 = findViewById(R.id.bt2);
+        rb1.setOnClickListener(this);
+        rb2.setOnClickListener(this);
+
+        fm = getSupportFragmentManager();
+
+
+        addApple();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.bt1){
+            addApple();
+        }
+        if(view.getId() == R.id.bt2){
+            addAndroid();
+        }
+    }
+
+    private void addApple(){
+        FragmentTransaction ft = fm.beginTransaction();
+        if(androidFragment != null){
+            ft.hide(androidFragment);
+        }
+        if(appleFragment == null){
+            appleFragment = new AppleFragment();
+            ft.add(R.id.frame, appleFragment, "apple");
+        }else {
+            ft.show(appleFragment);
+        }
+        ft.commitAllowingStateLoss();
+    }
+
+    private void addAndroid(){
+        FragmentTransaction ft = fm.beginTransaction();
+        if(appleFragment != null){
+            ft.hide(appleFragment);
+        }
+        if(androidFragment == null){
+            androidFragment = new AndroidFragment();
+            ft.add(R.id.frame, androidFragment, "android");
+        }else{
+            ft.show(androidFragment);
+        }
+        ft.commitAllowingStateLoss();
+    }
+}
---
+++ app/src/main/java/com/example/demo/activity/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.demo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.demo.R;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView(){
+        Button b1 = findViewById(R.id.b1);
+        b1.setOnClickListener(this);
+        Button b2 = findViewById(R.id.b2);
+        b2.setOnClickListener(this);
+        Button b3 = findViewById(R.id.b3);
+        b3.setOnClickListener(this);
+        Button b4 = findViewById(R.id.b4);
+        b4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.b1){
+            Intent intent = new Intent(this, OkhttpActivity.class);
+            startActivity(intent);
+        }
+        if(view.getId() == R.id.b2){
+            Intent intent = new Intent(this, BannerActivity.class);
+            startActivity(intent);
+        }
+        if(view.getId() == R.id.b3){
+            Intent intent = new Intent(this, RefreshAndLoadActivity.class);
+            startActivity(intent);
+        }
+        if(view.getId() == R.id.b4){
+            Intent intent = new Intent(this, FrameActivity.class);
+            startActivity(intent);
+        }
+    }
+}
